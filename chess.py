c6346303daa563b64842c583613f960d880cca72
dnzgo <dnzgzc.98@gmail.com>
2025-01-07 18:28:40 +0100

    added selection and moving pieces without rules

--- chess.py
+++ chess.py
@@ -10,4 +10,44 @@ class GameState:
             ["wP", "wP", "wP","wP","wP","wP","wP","wP","wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
-        self.current_player = "W"
+        self.current_player = "w"
+    
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_column] = "  "                    #clear the old location of piece
+        self.board[move.end_row][move.end_column]     = move.piece_moved        # put it to the new location
+        self.change_player()                                                    #swapping player after move
+
+    def change_player(self):
+        if self.current_player == "w":
+            self.current_player = "b"
+        else: self.current_player = "w"
+
+
+
+
+
+
+class Move:
+
+    ranks_to_rows     = {"1" : 7, "2" : 6, "3" : 5, "4" : 4,
+                         "5" : 3, "6" : 2, "7" : 1, "8": 0}
+    rows_to_ranks     = {v : k for k, v in ranks_to_rows.items()}
+    files_to_columns  = {"a" : 0, "b" : 1, "c" : 2, "d" : 3,
+                         "e" : 4, "f" : 5, "g" : 6, "h": 7}
+    columns_to_files  = {v : k for k, v in files_to_columns.items()}
+
+    def __init__(self, start_cell, end_cell, board):
+        self.start_row       = start_cell[0]
+        self.start_column    = start_cell[1]
+        self.end_row         = end_cell[0]
+        self.end_column      = end_cell[1]
+        self.piece_moved     = board[self.start_row][self.start_column]
+        self.piece_captured  = board[self.end_row][self.end_column]
+
+
+    def get_chess_notation(self):                   #pieces old cell and new cells locations as 'a2b3'
+        return self.get_rank_file(self.start_row, self.start_column) + self.get_rank_file(self.end_row, self.end_column)
+    
+    def get_rank_file(self, row, column):
+        return self.columns_to_files[column] + self.rows_to_ranks[row]
--- game.py
+++ game.py
@@ -11,38 +11,58 @@ board_image = pygame.image.load("images/board.png")
 def load_images():
     pieces = ["wP", "wB",  "wK", "wN", "wR", "wQ", "bP", "bB",  "bK", "bN", "bR", "bQ"]
     for piece in pieces:
-        images[piece] = pygame.image.load("images/" + piece +".png")
+        images[piece] = pygame.transform.scale(pygame.image.load("images/" + piece +".png"), (cell_size, cell_size))    # a dictionary for piece and piece image
 
 
 # handling user input and updating graphics
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((width, heigth))
-    pygame.display.set_caption("Chess by null")
+    screen = pygame.display.set_mode((width, heigth))       #arranging the screen size
+    pygame.display.set_caption("Chess by null")             #title of the screen
     game_state = chess.GameState()
     load_images()
     runnig = True
+    selected_cell = ()                                      # last click of user (row, col)
+    player_clicks = []                                      # players first and last click
     while runnig:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:                   # if user close the screen
                 runnig = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()           # getting the location of the click
+                column = location[0] // cell_size
+                row    = location[1] // cell_size
+                if selected_cell == (row, column):
+                    selected_cell = ()                      #if player clicks same cell twice  undo the selection
+                    player_clicks = []
+                else:
+                    selected_cell = (row, column)
+                    player_clicks.append(selected_cell)
+
+                if len(player_clicks) == 2:
+                    move = chess.Move(player_clicks[0], player_clicks[1], game_state.board)
+                    game_state.make_move(move)
+                    selected_cell = ()                      # after move clear the selection
+                    player_clicks = []                      # after move clear the clicks
         draw_game_state(screen, game_state)
         pygame.display.flip()
 
 # responsible for all the graphic changes
 def draw_game_state(screen, game_state):
-    draw_board(screen)                      # drawing squares on the board 
-    draw_pieces(screen, game_state.board)   # add the pieces on the board with current game state
+    draw_board(screen)                                      # drawing squares on the board 
+    draw_pieces(screen, game_state.board)                   # add the pieces on the board with current game state
 
 def draw_board(screen):
-    screen.blit(board_image, (0, 0))
+    screen.blit(board_image, (0, 0))                        # adding the background image to the game
 
 def draw_pieces(screen, board):
     for row in range(dimension):
         for column in range(dimension):
             piece = board[row][column]
-            if piece != "  ":
-                screen.blit(images[piece], pygame.Rect(column * cell_size, row * cell_size, cell_size, cell_size))
+            if piece != "  ":                               # checking if there is a piece on the cell and show its image
+                screen.blit(images[piece], pygame.Rect(column * cell_size,
+                                                        row * cell_size, 
+                                                        cell_size, cell_size))
 
 
 main()
