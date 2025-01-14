f996a87ca0ba7009b089989552bd0cd7bbad8025
dnzgo <dnzgzc.98@gmail.com>
2025-01-14 20:34:08 +0100

    added highlight

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ dimension   = 8
 cell_size   = width // dimension
 images      = {}
 board_image = pygame.transform.scale(pygame.image.load("images/board.png"), (width, heigth))
+highlight_img = pygame.transform.scale(pygame.image.load("images/green_circle.png"), (cell_size, cell_size))
 
 def load_images():
     pieces = ["wP", "wB",  "wK", "wN", "wR", "wQ", "bP", "bB",  "bK", "bN", "bR", "bQ"]
@@ -26,6 +27,7 @@ def main():
     runnig = True
     selected_cell = ()                                      # last click of user (row, col)
     player_clicks = []                                      # players first and last click
+    moveable_cells = []
     while runnig:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:                   # if user close the screen
@@ -40,23 +42,36 @@ def main():
                     print("you unselect the piece")
                     selected_cell = ()                      #if player clicks same cell twice  undo the selection
                     player_clicks = []
+                    moveable_cells = []
                 else:
                     selected_cell = (row, column)
                     player_clicks.append(selected_cell)     # add new click to the list
 
-                if len(player_clicks) == 2:                 # if the length of list is 2 we can make move
+                # If the first click selects a piece, calculate valid moves
+                if len(player_clicks) == 1:
+                    piece = game_state.create_piece(game_state.board[row][column])
+                    if piece and piece.color == game_state.current_player:
+                        piece.get_valid_moves(player_clicks[0],game_state.board)  # Calculate valid moves
+                        moveable_cells = piece.valid_moves               # Store valid moves for highlighting
+                    else:
+                        moveable_cells = []  # Clear valid moves if no valid piece is selected
+
+                elif len(player_clicks) == 2:                 # if the length of list is 2 we can make move
                     move = chess.Move(player_clicks[0], player_clicks[1], game_state.board)
                     game_state.make_move(move)
                     selected_cell = ()                      # after move clear the selection
                     player_clicks = []                      # after move clear the clicks
-        draw_game_state(screen, game_state, selected_cell)
+                    moveable_cells = []
+        draw_game_state(screen, game_state, selected_cell, moveable_cells)
         pygame.display.flip()                               # update the screen
 
 # responsible for all the graphic changes
-def draw_game_state(screen, game_state, selected_cell):
+def draw_game_state(screen, game_state, selected_cell, moveable_cells):
     draw_board(screen)                                      # drawing squares on the board 
     draw_selected_cell(screen, selected_cell)               # Highlight the selected cell
     draw_pieces(screen, game_state.board)                   # add the pieces on the board with current game state
+    if moveable_cells != []:
+        highlight_moves(screen, moveable_cells) 
 
 def draw_board(screen):
     screen.blit(board_image, (0, 0))                        # adding the background image to the game
@@ -76,6 +91,18 @@ def draw_selected_cell(screen, selected_cell):
         rect = pygame.Rect(column * cell_size, row * cell_size, cell_size, cell_size)
         pygame.draw.rect(screen, (255, 255, 255), rect, 3)       # Yellow border with thickness 3
 
-
+def highlight_moves(screen, moveable_cells):
+    for move in moveable_cells:
+        row, column = move  # Unpack the row and column from each move
+        # Draw a green rectangle on the valid cell
+        screen.blit(highlight_img, (column * cell_size, row * cell_size, cell_size, cell_size))
+        '''
+        pygame.draw.rect(
+            screen,
+            (0, 255, 0, 128),  # RGBA color: Green with 50% transparency
+            pygame.Rect(column * cell_size, row * cell_size, cell_size, cell_size)
+        )
+        '''
+        
 
 main()
